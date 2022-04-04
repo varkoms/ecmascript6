75e029485c0b1323a0febb8c05712ecb3f7ff618
Cesar Soberanis <cmendozasoberanis@gmail.com>
2022-04-04 10:46:27 -0500

    ECMAScript 2020 (ES11) - Caracteristicas Importantes

--- src/es11/index.js
+++ src/es11/index.js
@@ -9,3 +9,42 @@ button.addEventListener('click', async function() {
   const module = await import("./file.js");
   module.hello();
 });
+
+// Big Int
+
+const aBigNumber = 9007199254740991n;
+const anotherBigIntNumber = BigInt(9007199254740991);
+
+console.log(aBigNumber);
+console.log(anotherBigIntNumber);
+
+// Promise.allSettled()
+// La diferencia entre allSettled y all, es que all ejecuta las promesas, pero si falla una, se detiene
+// en cambio allSettled, ejecuta todas las promesas pero no se detiene, completa todas y envia los resultados
+
+const promise1 = new Promise((resolve, reject) => reject('reject'));
+const promise2 = new Promise((resolve, reject) => resolve('resolve'));
+const promise3 = new Promise((resolve, reject) => resolve('resolve 1'));
+
+Promise.allSettled([promise1, promise2, promise3])
+  .then(response => console.log(response));
+
+// Global This
+console.log(window); // Solo funciona con el browser, en consola no
+console.log(globalThis);
+
+// Nullish coalescing operator
+const foo = 'null' ?? 'default string';
+console.log(foo);
+
+// Optional chaining (?.)
+// Previene que la aplicacion se rompa si no existen valores
+
+const user = {}; // Pensando que este objeto sera llenado mas adelante
+console.log(user?.profile?.email);
+
+if (user?.profile?.email) {
+  console.log('email');
+} else {
+  console.log('fail');
+}
